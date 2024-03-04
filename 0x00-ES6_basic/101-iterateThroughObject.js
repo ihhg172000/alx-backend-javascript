3b33c76a810886d877c12f3de28e85d97f855e68
Islam Hany <ihhg.developer@gmail.com>
2024-03-04 18:40:23 +0200

    14. Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee)
+  }
+
+  return employees.join(' | ');
+}
