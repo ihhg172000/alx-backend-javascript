2066122605842659fb2d31040c04c93c1b539161
Islam Hany <ihhg.developer@gmail.com>
2024-03-06 14:36:43 +0200

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     uploadPhoto(fileName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.value || result.reason,
+    value: result.value || String(result.reason),
   })));
 }
